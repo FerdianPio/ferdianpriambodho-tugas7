0168d996b08ea15f44301ea1d9bc2ed288f90957
FerdianPio <76025616+FerdianPio@users.noreply.github.com>
2022-08-29 13:36:51 +0700

    solve request 4
    - add pool logic to MushroomSpawner.cs.
    - add object lifeCycle to Mushroom.cs so SpawnedMushroom can disable when lifecycle ended.

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -6,6 +6,7 @@ public class Mushroom : MonoBehaviour
 {
     public float upForce = 1f;
     public float sideForce = .1f;
+    public float lifeCycle = 3f;
     void Start()
     {
         float xForce = Random.Range(-sideForce, sideForce);
@@ -15,5 +16,17 @@ public class Mushroom : MonoBehaviour
         Vector3 force = new Vector3 (xForce, yForce, zForce);
 
         GetComponent<Rigidbody>().velocity = force;
+    }
+    void Update()
+    {
+        if (lifeCycle >= 0 && gameObject.activeInHierarchy)
+        {
+            lifeCycle -= Time.deltaTime;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            lifeCycle = 3f;
+        }
     }
 }
--- Assets/Scripts/MushroomSpawner.cs
+++ Assets/Scripts/MushroomSpawner.cs
@@ -5,8 +5,36 @@ using UnityEngine;
 public class MushroomSpawner : MonoBehaviour
 {
     public GameObject objPrefab;
-    
-    void FixedUpdate(){
-        Instantiate(objPrefab, transform.position, Quaternion.identity);
+    [SerializeField] int _poolSize;
+    private List<GameObject> _pool = new List<GameObject>();
+
+    void Start()
+    {
+        for(int i = 0; i < _poolSize; i++)
+        {
+            _pool.Add(Instantiate(objPrefab, transform.position, Quaternion.identity));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        GameObject tmpPoolObject = FindInactivePoolObject();
+        if (tmpPoolObject != null)
+        {
+            tmpPoolObject.transform.position = transform.position;
+            tmpPoolObject.SetActive(true);
+        }
+    }
+
+    private GameObject FindInactivePoolObject()
+    {
+        foreach(GameObject go in _pool)
+        {
+            if (!go.activeInHierarchy)
+            {
+                return go;
+            }
+        }
+        return null;
     }
 }
